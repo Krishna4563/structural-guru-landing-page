e73089ae06aeefdf8676c5bb23f1b8bdb951f32d
Krishna4563 <daskrishna0902@gmail.com>
2023-11-30 19:30:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Hero from "../src/components/hero";
+import React from "react";
+
+function App() {
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/hero.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./hero.css";
+import img1 from "../assets/img1.webp";
+
+const Hero = () => {
+  return (
+    <section>
+      <img src={img1} alt="" width={100} height={100} />
+      <p className="quotes-1">
+        WELCOME TO PRACTICAL STRUCTURAL DESIGN CONSULTANCY TRAINING
+      </p>
+
+      <div className="quotes">
+        <p className="quotes-2">
+          HERE IS YOUR GATEWAY FOR LEARNING STRUCTURAL DESIGN IN PRACTICAL WAY !
+        </p>
+      </div>
+
+      <div className="course-main-div">
+        <div className="left-div">
+          <h1 className="left-title"></h1>
+          <div className="left-content"></div>
+        </div>
+
+        <div className="center-line"></div>
+
+        <div className="right-div">
+          <h1 className="right-title"></h1>
+          <div className="right-content"></div>
+        </div>
+      </div>
+
+      <div className="video-titles-div">
+        <h1 className="yt-title1">
+          ABOUT OUR PRACTICAL STRUCTURAL DESIGN CONSULTANCY TRAINING
+        </h1>
+        <div className="center-line-2"></div>
+        <h2 className="yt-title2">Watch this explanation of this training!</h2>
+      </div>
+
+      <div className="video-div">
+        <iframe
+          width="900"
+          height="550"
+          src="https://www.youtube.com/embed/0npY5ja2xP8?autoplay=1"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
+
+      <div className="motto-quote">
+        <p>OUR MOTTO: HELP EACH OTHER ! GROW TOGETHER !! </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
